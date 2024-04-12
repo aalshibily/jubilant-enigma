afb7f71200021041df3976f1d66b71330825dba5
Abdullah Alshibily <aalshibaily@live.com>
2024-04-11 23:12:31 -0500

    Weather interface

---
+++ Aegis.Core/Interfaces/Services/IWeatherService.cs
@@ -0,0 +1,14 @@
+﻿using Aegis.Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aegis.Core.Interfaces.Services
+{
+    public interface IWeatherService
+    {
+        Task<IEnumerable<ActiveAlert>> GetAlertsByState(string state);
+    }
+}
